d4a2eaf629c4f613e6231ed366a1455483d6e94a
Jonas Karlman <jonas@kwiboo.se>
2017-11-01 22:57:41 +0100

    linux: add rockchip-4.4

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -45,6 +45,13 @@ case "$LINUX" in
     PKG_SOURCE_DIR="$PKG_NAME-amlogic-$PKG_VERSION*"
     PKG_PATCH_DIRS="amlogic-3.14"
     ;;
+  rockchip-4.4)
+    PKG_VERSION="a1af913e"
+    PKG_SHA256="09460e2fd0b8cdcd081ec7f7f465e4329f39e4f0ddc630d58f7a81af46074805"
+    PKG_URL="https://github.com/rockchip-linux/kernel/archive/$PKG_VERSION.tar.gz"
+    PKG_SOURCE_DIR="kernel-$PKG_VERSION*"
+    PKG_PATCH_DIRS="rockchip-4.4"
+    ;;
   *)
     PKG_VERSION="4.14"
     PKG_SHA256="f81d59477e90a130857ce18dc02f4fbe5725854911db1e7ba770c7cd350f96a7"
